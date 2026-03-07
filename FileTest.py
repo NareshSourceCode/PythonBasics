f7566e310cbc52b33a690e66fd60754c6bb8ac0a
Naresh Goud <naresh.goud779@gmail.com>
2026-03-07 11:14:55 +0530

    Some more basics

---
+++ FileTest.py
@@ -0,0 +1,3 @@
+ 
+with open('E:\GenAI\PyBasics\PythonBasics\my_file.txt','r') as myFile:
+    print(myFile.read())
---
+++ ForLoop.py
@@ -0,0 +1,27 @@
+mLst =[1, 3,4,5,6,7,8,9]
+for i in mLst:
+    if(i%2==0):# printing only even numbers
+        print(f"{i} is even number") # printing even numbers with a message
+    elif(i%2!=0): # printing only odd numbers   
+        print(f'{i} is odd number') # printing odd numbers with a message
+
+sum =1
+for i in range(1,9): # printing numbers from 1 to 10
+   # print(i) # printing numbers from 1 to 10
+    sum =sum+i
+print(f"The sum of first 10 natural numbers is {sum}") # printing the sum of first 10 natural numbers
+
+mTupleList = [(1,2),(3,4),(5,6)]
+for i in mTupleList:
+    print(i) # printing the tuple list
+for (a,b) in mTupleList:
+    print(f"{a}  ") # printing the tuple list
+    print(f"{b}") # printing the tuple list
+
+mydict = {'name':'John','age':30,'city':'New York'}
+for key in mydict: # printing the dictionary values
+    print(key) # printing the dictionary values
+for key in mydict: # printing the dictionary values
+    print(mydict[key]) # printing the dictionary values
+for key, value in mydict.items(): # printing the dictionary values
+    print(f"{key} : {value}") # printing the dictionary values
